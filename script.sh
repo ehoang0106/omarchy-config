ec5ab351ba4ee8f0ac5e841c7fd8dfff0a65ff43
ehoang0106 <khoahoang1698@gmail.com>
2025-11-21 12:12:59 -0800

    backup style and conf.jsonc

--- script.sh
+++ script.sh
@@ -32,3 +32,34 @@ print_banner(){
   echo -e "$ASCII_ART"
 }
 
+print_banner
+
+echo -e "${GREEN}Backing up existing Waybar config files..."
+
+#backup the exsiting config.jsonc and style.css in ~/.config/waybar/
+WAYBAR_CONFIG_DIR="$HOME/.config/waybar"
+mv $WAYBAR_CONFIG_DIR/config.jsonc $WAYBAR_CONFIG_DIR/config.jsonc.bak
+mv $WAYBAR_CONFIG_DIR/style.css $WAYBAR_CONFIG_DIR/style.css.bak
+
+
+
+#clone the git repo contains waybar config files and kitty config files
+GITHUB_REPO_URL="git@github.com:ehoang0105/omarchy-config.git"
+cd $HOME
+mkdir omarchy-config-script && cd omarchy-config-script
+git clone $GITHUB_REPO_URL
+cd omarchy-config
+
+
+
+
+
+
+
+
+
+
+
+
+
+
